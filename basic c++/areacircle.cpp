c2ce3846cf6a7180ae349a1efeebbbb41bf92216
yashika tyagi <yashikatyagi2311@gmail.com>
2026-07-30 10:28:40 +0530

    Added new C++ program

---
+++ basic c++/areacircle.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float r;
+    cin >> r;
+    cout << "Area = " << 3.14159 * r * r;
+    return 0;
+}
---
+++ basic c++/arearectangle.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int length = 10, width = 5;
+    cout << "Area = " << length * width;
+    return 0;
+}
