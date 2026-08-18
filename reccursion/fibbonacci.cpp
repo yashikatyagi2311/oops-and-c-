b3842b394a3b33920b9da8ce88456febbf09cd26
yashika tyagi <yashikatyagi2311@gmail.com>
2026-08-18 09:08:12 +0530

    add

--- reccursion/fibbonacci.cpp
+++ reccursion/fibbonacci.cpp
@@ -3,9 +3,9 @@ using namespace std;
 
 // Recursive function to calculate nth Fibonacci number
 int fibonacci(int n) {
-    if (n == 0)   // base case
+    if (n == 0)   
         return 0;
-    else if (n == 1)   // base case
+    else if (n == 1)   
         return 1;
     else
         return fibonacci(n - 1) + fibonacci(n - 2);  // recursive call
