9ac1c17db9f9ec0726eb81590228405131547ddf
yashika tyagi <yashikatyagi2311@gmail.com>
2026-08-19 15:36:42 +0530

    add

--- reccursion/factorial.cpp
+++ reccursion/factorial.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 using namespace std;
 
-// Recursive function to calculate factorial
+
 int factorial(int n) {
-    if (n == 0 || n == 1)   // base case
+    if (n == 0 || n == 1)   
         return 1;
     else
-        return n * factorial(n - 1);  // recursive call
+        return n * factorial(n - 1);  
 }
 
 int main() {
