40d98fd7dfd4903f914dcb349b121150ee7024d1
yashika tyagi <yashikatyagi2311@gmail.com>
2026-08-19 15:13:39 +0530

    intersection

---
+++ array/intersectionofarrays.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+void intersectionArrays(int arr1[], int n1, int arr2[], int n2) {
+    int i = 0, j = 0;
+
+    cout << "Intersection of arrays: ";
+
+    
+    while (i < n1 && j < n2) {
+        if (arr1[i] < arr2[j]) {
+            i++;
+        } else if (arr2[j] < arr1[i]) {
+            j++;
+        } else {
+        
+            cout << arr1[i] << " ";
+            i++;
+            j++;
+        }
+    }
+
+    cout << endl;
+}
+
+int main() {
+    int arr1[] = {1, 2, 4, 5, 7};
+    int arr2[] = {2, 3, 5, 6};
+
+    int n1 = sizeof(arr1) / sizeof(arr1[0]);
+    int n2 = sizeof(arr2) / sizeof(arr2[0]);
+
+    intersectionArrays(arr1, n1, arr2, n2);
+
+    return 0;
+}
