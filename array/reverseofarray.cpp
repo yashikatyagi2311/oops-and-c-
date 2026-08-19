20d64a41767832b6b16b4a71cf6937c5c46b3541
yashika tyagi <yashikatyagi2311@gmail.com>
2026-08-19 15:17:26 +0530

    add

---
+++ array/reverseofarray.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+void reverseArray(int arr[], int n) {
+    int start = 0, end = n - 1;
+
+    
+    while (start < end) {
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+
+        start++;
+        end--;
+    }
+}
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    reverseArray(arr, n);
+
+    cout << "Reversed Array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
